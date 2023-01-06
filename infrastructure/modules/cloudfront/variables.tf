d33ae6ea8675bba56ad4e55186f18a310b6d85ca
hoaftq <hoaftq@gmail.com>
2023-01-06 23:46:16 +0800

    Uploaded files with terraform, separated environments

--- infrastructure/modules/cloudfront/variables.tf
+++ infrastructure/modules/cloudfront/variables.tf
@@ -2,3 +2,8 @@ variable "origin_id" {
   description = "bucket origin ID"
   type        = string
 }
+
+variable "upload_path" {
+  description = "Path of the directory that will be uploaded to the bucket"
+  type        = string
+}
